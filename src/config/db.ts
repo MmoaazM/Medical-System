2ac7d741cf544759c0fc5e94d0f93c36b1674e90
MmoaazM <imoaaz29@gmail.com>
2026-08-23 14:23:42 +0300

    Finish Required Configurations

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,14 @@
 import mongoose from "mongoose";
 
 const connectDB = async (): Promise<void> => {
- 
+    try {
+        await mongoose.connect(process.env.MONGO_URI as string)
+        console.log("Connected To Mongo Successfully");
+        
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        process.exit(1);
+    }
 };
 
 export default connectDB;
--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -1,7 +1,35 @@
 import swaggerJSDoc from "swagger-jsdoc";
 
-const swaggerOptions = {
- 
+const swaggerOptions: swaggerJSDoc.Options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "🩺 Medical Appointment System API Documentation",
+      version: "1.0.0",
+      description: "Comprehensive REST API for Patients, Doctors, and Admin scheduling & appointments.",
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/api",
+        description: "Development Server",
+      },
+    ],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
+        },
+      },
+    },
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
+  },
+  apis: ["./src/routes/*.ts" , "./dist/routes/*.js"],
 };
 
 export const swaggerSpec = swaggerJSDoc(swaggerOptions);
