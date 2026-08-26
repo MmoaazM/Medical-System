0e6d970538bce3fcfd2bca0756c0b7a78d5f2284
omar ibrahim <mmaro5584@gmail.com>
2026-08-27 01:44:57 +0300

    added user model

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,8 +2,17 @@ import { Schema, model } from "mongoose";
 
 export type UserRole = "Patient" | "Doctor" | "Admin";
 
+
 const userSchema = new Schema({
-  
+
+    FullName:String,
+    Email:String,
+    Password:String,
+    Role: {
+    type: String,
+    enum: ["Patient", "Doctor", "Admin"],
+    required: true,
+  }
 });
 
 export default model("User", userSchema);
